0eab4845e0d16c71981d756ec5c44c28bd41b5d6
Apprentice <apprentice@devbootcamp.com>
2013-04-24 09:13:06 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DbcOverflow::Application.config.secret_token = '4a1340150e9b8027c34de575e3899f837684d765a738f56c12343ea5b10096d5b60283b1b6384f7506dccf5abc14258c2eb4df8c0f8c557d9be4a8397302da4a'
