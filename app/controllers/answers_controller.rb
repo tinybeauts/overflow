0581139c8ac76ba17aaf4c57817643b4396b1f44
Apprentice <apprentice@devbootcamp.com>
2013-04-25 14:24:36 -0700

    users can edit and delete their own answers

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,4 +1,5 @@
 class AnswersController < ApplicationController
+
   def create
     @question = Question.find(params[:question_id])
     @answer = Answer.new(params[:answer])
@@ -18,7 +19,8 @@ class AnswersController < ApplicationController
   end
   
   def edit
-
+    @answer = Answer.find(params[:id])
+    @question = Question.find(params[:question_id])
   end
   
   def show
@@ -26,10 +28,22 @@ class AnswersController < ApplicationController
   end
   
   def update
+    @answer = Answer.find(params[:id])
+    @question = Question.find(params[:question_id])
 
+    if @answer.update_attributes(params[:answer])
+      flash[:success] = "Your answer was updated!"
+      redirect_to @question
+    else
+      flash[:error] = "you suck"
+      redirect_to edit_question_answer_path(@question, @answer)
+    end
   end
 
   def destroy
-
+    @question = Question.find(params[:question_id])
+    Answer.find(params[:id]).destroy
+    flash[:success] = "BOOM"
+    redirect_to @question
   end
 end
